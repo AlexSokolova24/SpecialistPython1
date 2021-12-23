f94086b75268ae950aeb87183b0ecf4a09cbd416
AlexSokolova24 <96444541+AlexSokolova24@users.noreply.github.com>
2021-12-23 11:29:07 +0300

    Update 01_task_dict.py

--- Module3/practice/dict/01_task_dict.py
+++ Module3/practice/dict/01_task_dict.py
@@ -4,7 +4,18 @@
 # Считая,что курс доллара равен dollar_rate
 # Вычислите цену всех товаров с названием "name" в долларах
 
-item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "count": "10"}
+item = {
+    "name": "Кроссовки",
+    "price": "7500",
+    "currency": "rub",
+    "count": "10",
+}
+
+price = int(item["price"])
+count = int(item["count"])
 dollar_rate = 74.12
+summ_all_item = price * count
+summ_in_dollar = summ_all_item/dollar_rate
+print(summ_in_dollar)
 
 # TODO: your code here
